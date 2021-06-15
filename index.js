08f6a1022532b1b04b42ecb0dce29f283bd22997
Rick <rmansfield@realpeoplesratings.com>
2021-06-15 13:05:53 -0500

    confirmational push 5 failed and 1 passed 6 total

--- index.js
+++ index.js
@@ -4,23 +4,24 @@ import { fifaData } from './fifa.js';
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
-const finals2024 = fifaData.filter(function(item){ return item.Year === 2014 && item.Stage === 'Final';
+const finals2014 = fifaData.filter(function(item){ 
+    return item.Year === 2014 && item.Stage === 'Final';
 });
-console.log('task 1', finals2024);
+console.log('task 1', finals2014);
 //(a) Home Team name for 2014 world cup final
 console.log('task 1a', finals2014[0]['Home Team Name']);
 
 //(b) Away Team name for 2014 world cup final
-console.log('task 1b', finals2024[0]['Away Team Name']);
+console.log('task 1b', finals2014[0]['Away Team Name']);
 
 //(c) Home Team goals for 2014 world cup final
-console.log('task 1c', finals2024[0]['Home Team Goals']);
+console.log('task 1c', finals2014[0]['Home Team Goals']);
 
 //(d) Away Team goals for 2014 world cup final
-console.log('task 1d', finals2024[0]['Away Team Goals']);
+console.log('task 1d', finals2014[0]['Away Team Goals']);
 
 //(e) Winner of 2014 world cup final */
-console.log('task 1e', finals2024[0]['Win conditions']);
+console.log('task 1e', finals2014[0]['Win conditions']);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
